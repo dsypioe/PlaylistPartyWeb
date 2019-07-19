297a2dfd0c0d5a4ce0749dc988dabfa78d63ea48
speacock103 <50715076+speacock103@users.noreply.github.com>
2019-07-19 17:09:27 -0400

    Add files via upload

---
+++ api/addtoBlacklist.php
@@ -0,0 +1,44 @@
+<?php
+	$inData = getRequestInfo();
+	
+	$roomid = $inData["roomid"];
+	$songid = $inData["songid"];
+	
+	// connects to database and inserts data into room table for created room
+	$conn = new mysqli("localhost", "v3ksrrem0t05", "#Ijsda914", "PlaylistParty");
+	if ($conn->connect_error) 
+	{
+		returnWithError( $conn->connect_error );
+	} 
+	else
+	{
+		$sql = "INSERT INTO Blacklist (roomid, songid) VALUES ('" . $roomid . "','" . $songid . "')";
+		if( $result = $conn->query($sql) != TRUE )
+		{
+			returnWithError( $conn->error );
+		}
+		$conn->close();
+	}
+	
+	returnWithError("");
+	
+	// get json data
+	function getRequestInfo()
+	{
+		return json_decode(file_get_contents('php://input'), true);
+	}
+
+    // send json
+	function sendResultInfoAsJson( $obj )
+	{
+		header('Content-type: application/json');
+		echo $obj;
+	}
+	
+	// return json with error message
+	function returnWithError( $err )
+	{
+		$retValue = '{"error":"' . $err . '"}';
+		sendResultInfoAsJson( $retValue );
+	}
+?>
